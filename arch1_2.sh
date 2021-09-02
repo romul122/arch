cbf666d8aaf3302acc5d097e215c9d96f97c39e1
Romul <37873384+romul122@users.noreply.github.com>
2021-09-02 13:18:47 +0300

    Update arch1_2.sh

--- arch1_2.sh
+++ arch1_2.sh
@@ -2,56 +2,56 @@
 read -p "Введите имя компьютера: " hostname
 read -p "Введите имя пользователя: " username
 
-echo 'Ïðîïèñûâàåì èìÿ êîìïüþòåðà'
+echo 'Прописываем имя компьютера'
 echo $hostname > /etc/hostname
 ln -svf /usr/share/zoneinfo/Asia/Yekaterinburg /etc/localtime
 
-echo '3.4 Äîáàâëÿåì ðóññêóþ ëîêàëü ñèñòåìû'
+echo '3.4 Добавляем русскую локаль системы'
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen 
 
-echo 'Îáíîâèì òåêóùóþ ëîêàëü ñèñòåìû'
+echo 'Обновим текущую локаль системы'
 locale-gen
 
-echo 'Óêàçûâàåì ÿçûê ñèñòåìû'
+echo 'Указываем язык системы'
 echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
 
-echo 'Âïèñûâàåì KEYMAP=ru FONT=cyr-sun16'
+echo 'Вписываем KEYMAP=ru FONT=cyr-sun16'
 echo 'KEYMAP=ru' >> /etc/vconsole.conf
 echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
 
-echo 'Ñîçäàäèì çàãðóçî÷íûé RAM äèñê'
+echo 'Создадим загрузочный RAM диск'
 mkinitcpio -p linux
 
-echo '3.5 Óñòàíàâëèâàåì çàãðóç÷èê'
+echo '3.5 Устанавливаем загрузчик'
 pacman -Syy
 pacman -S grub --noconfirm 
 grub-install /dev/sda
 
-echo 'Îáíîâëÿåì grub.cfg'
+echo 'Обновляем grub.cfg'
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo 'Ñòàâèì ïðîãðàììó äëÿ Wi-fi'
+echo 'Ставим программу для Wi-fi'
 pacman -S dialog wpa_supplicant --noconfirm 
 
-echo 'Äîáàâëÿåì ïîëüçîâàòåëÿ'
+echo 'Добавляем пользователя'
 useradd -m -g users -G wheel -s /bin/bash $username
 
-echo 'Ñîçäàåì root ïàðîëü'
+echo 'Создаем root пароль'
 passwd
 
-echo 'Óñòàíàâëèâàåì ïàðîëü ïîëüçîâàòåëÿ'
+echo 'Устанавливаем пароль пользователя'
 passwd $username
 
-echo 'Óñòàíàâëèâàåì SUDO'
+echo 'Устанавливаем SUDO'
 echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
 
-echo 'Ðàñêîììåíòèðóåì ðåïîçèòîðèé multilib Äëÿ ðàáîòû 32-áèòíûõ ïðèëîæåíèé â 64-áèòíîé ñèñòåìå.'
+echo 'Раскомментируем репозиторий multilib Для работы 32-битных приложений в 64-битной системе.'
 echo '[multilib]' >> /etc/pacman.conf
 echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
 pacman -Syy
 
-echo "Êóäà óñòàíàâëèâåì Arch Linux íà âèðòóàëüíóþ ìàøèíó?"
+echo "Куда устанавливем Arch Linux на виртуальную машину?"
 read -p "1 - Äà, 0 - Íåò: " vm_setting
 if [[ $vm_setting == 0 ]]; then
   gui_install="xorg-server xorg-drivers xorg-xinit"
@@ -59,26 +59,26 @@ elif [[ $vm_setting == 1 ]]; then
   gui_install="xorg-server xorg-drivers xorg-xinit virtualbox-guest-utils"
 fi
 
-echo 'Ñòàâèì èêñû è äðàéâåðà'
+echo 'Ставим иксы и драйвера'
 pacman -S $gui_install
 
-echo "Ñòàâèì I3-WM"
+echo "Ставим I3-WM"
 pacman -S i3-wm i3-gaps i3status sbxkb dmenu pcmanfm ttf-font-awesome feh lxappearance thunar gvfs udiskie xorg-xbacklight ristretto tumbler picom --noconfirm
 
-echo 'Còàâèì DM'
+echo 'Cтавим DM'
 pacman -S sddm --noconfirm
 systemctl enable lxdm
 
-echo 'Ñòàâèì øðèôòû'
+echo 'Ставим шрифты'
 pacman -S ttf-liberation ttf-dejavu --noconfirm 
 
-echo 'Ñòàâèì ñåòü'
+echo 'Ставим сеть'
 pacman -S networkmanager network-manager-applet ppp --noconfirm
 
-echo 'Ïîäêëþ÷àåì àâòîçàãðóçêó ìåíåäæåðà âõîäà è èíòåðíåò'
+echo 'Подключаем автозагрузку менеджера входа и интернет'
 systemctl enable NetworkManager
 
-echo 'Óñòàíîâêà çàâåðøåíà! Ïåðåçàãðóçèòå ñèñòåìó.'
-echo 'Åñëè õîòèòå ïîäêëþ÷èòü AUR, òîãäà ïîñëå ïåðåçàãðçêè è âõîäà â ñèñòåìó, óñòàíîâèòå wget (sudo pacman -S wget) è âûïîëíèòå êîìàíäó:'
+echo 'Установка завершена! Перезагрузите систему.'
+echo 'Если хотите подключить AUR, тогда после перезагрзки и входа в систему, установите wget (sudo pacman -S wget) и выполните команду:'
 echo 'wget git.io/arch1_3.sh && sh arch1_3.sh'
 exit
